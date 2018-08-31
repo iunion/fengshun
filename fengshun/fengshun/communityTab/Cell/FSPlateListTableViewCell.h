2e550a0fb9993512ee4132daa3eb86992a02b7db
best2wazi <517526470@qq.com>
2018-08-31 16:58:52 +0800

    板块列表cell

---
+++ fengshun/fengshun/communityTab/Cell/FSPlateListTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  FSPlateListTableViewCell.h
+//  fengshun
+//
+//  Created by best2wa on 2018/8/31.
+//  Copyright © 2018年 FS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FSPlateListTableViewCell : UITableViewCell
+
+@end
