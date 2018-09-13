4eee173a3570e2ee4f24bd67450da5f26448a88e
DennisDeng <iunion@live.cn>
2018-09-13 14:17:37 +0800

    file

---
+++ fengshun/fengshun/userTab/mine/FSMyCommentVC.h
@@ -0,0 +1,13 @@
+//
+//  FSMyCommentVC.h
+//  fengshun
+//
+//  Created by jiang deng on 2018/9/13.
+//  Copyright © 2018年 FS. All rights reserved.
+//
+
+#import "FSTableViewVC.h"
+
+@interface FSMyCommentVC : FSTableViewVC
+
+@end
