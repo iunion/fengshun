3b873b13f0ea24c8647b925155821bf64e813786
AiweiChujian <oovsxx@163.com>
2018-09-17 12:47:25 +0800

    alertcontroller bug
    alertcontroller bug

---
+++ fengshun/fengshun/mainTab/FSOCRSearchResultVC.m
@@ -0,0 +1,37 @@
+//
+//  FSOCRSearchResultVC.m
+//  fengshun
+//
+//  Created by Aiwei on 2018/9/17.
+//  Copyright © 2018 FS. All rights reserved.
+//
+
+#import "FSOCRSearchResultVC.h"
+
+@interface FSOCRSearchResultVC ()
+
+@end
+
+@implementation FSOCRSearchResultVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
