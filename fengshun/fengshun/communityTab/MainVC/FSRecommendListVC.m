e511df98dd0be3ebb41306ad81f61f8e516d4515
gongxujie <517526470@qq.com>
2019-02-20 11:10:06 +0800

    提交错了一个东西

--- fengshun/fengshun/communityTab/MainVC/FSRecommendListVC.m
+++ fengshun/fengshun/communityTab/MainVC/FSRecommendListVC.m
@@ -121,8 +121,7 @@
 {
     [tableView deselectRowAtIndexPath:indexPath animated:NO];
     FSTopicModel *model = self.m_DataArray[indexPath.row];
-//    [FSPushVCManager showTopicDetail:[self.view.superview bm_viewController] topicId:model.m_Id];
-    [FSPushVCManager showAuth:[self.view.superview bm_viewController] type:0];
+    [FSPushVCManager showTopicDetail:[self.view.superview bm_viewController] topicId:model.m_Id];
 }
 
 @end
